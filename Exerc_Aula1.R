2df3114041622c079efdceca836bfb03ce6a0ef7
Nathalie Taylor <108550456+nathalietaylor@users.noreply.github.com>
2023-03-29 16:03:31 -0300

    Add files via upload

---
+++ Exerc_Aula1.R
@@ -0,0 +1,34 @@
+#Exercícios Aula 1
+
+#Exercício 1: calculadora
+2 * 2 - (4 + 4) / 2
+
+#resto da divisão de 5 por 3
+5 %% 3
+
+
+# parte inteira da divisão de 5 por 3
+5 %/% 3  
+
+#Numero de ouro: 
+# (1+ raiz de 5)/2
+
+raiz_de_5 <- 5^(1/2)
+
+num_de_ouro <- (1+raiz_de_5)/2
+
+#ou a expressão completa
+(1+5^(1/2))/2
+ 
+#ou, raiz quadrada = sqrt
+(1+sqrt(5))/2
+
+
+
+#Exercício 2: objetos
+
+#multiplique sua idade por 12 w salve o resultado como "idade_em_meses".
+#em seguida multiplique esse objeto por trinta e salve como "idade_em_dias".
+
+idade_em_meses <- 29*12
+idade_em_dias <- idade_em_meses*30
